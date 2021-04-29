c4a0e150ef0e86e4745c41559932b330e6ea6e1a
Melissa Albarella <moexu13@gmail.com>
2021-04-28 19:20:15 -0700

    added logging

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const logger = require("./config/logger");
 
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(logger);
 
 app.use("/projects", projectsRouter);
 
